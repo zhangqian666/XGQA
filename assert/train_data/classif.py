80cac6d399d0ba6b9bd375a9d761fe51afe06733
zhangqian <415677303@qq.com>
2020-08-18 18:02:49 +0800

    修改训练

--- assert/train_data/classif.py
+++ assert/train_data/classif.py
@@ -7,18 +7,29 @@
 
 @Created on: 2020-08-14 18:08
 """
+import random
 
 with open("/Users/zhangqian/jupyer-notebookproject/XGQA/assert/train_data/" + "dev" + ".tsv", "r",
           encoding="utf-8") as f:
     question = ""
     numlist = []
     for line in f.readlines():
-        str = line.split("\t")[1]
+        numlist.append(line)
 
-        # print("{} ----- {} ".format(str, num))
-        if str not in numlist:
-            numlist.append(str)
+    random.shuffle(numlist)
 
-        else:
-            print("重复数据： --------------------》  {}", format(str))
+    with open("/Users/zhangqian/jupyer-notebookproject/XGQA/assert/train_data/" + "dev1" + ".tsv", "w",
+              encoding="utf-8") as f1:
 
+        for line1 in numlist:
+            f1.write(line1)
+
+    # str = line.split("\t")[1]
+    #
+    # # print("{} ----- {} ".format(str, num))
+    # if str not in numlist:
+    #     numlist.append(str)
+    #
+    # else:
+    #     print("重复数据： --------------------》  {}", format(str))
+    #
