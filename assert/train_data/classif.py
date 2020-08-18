8e94f872a7c98bd3a88e59ac0a375612e3fb8a33
zhangqian <415677303@qq.com>
2020-08-18 17:22:25 +0800

    修改训练

--- assert/train_data/classif.py
+++ assert/train_data/classif.py
@@ -8,20 +8,17 @@
 @Created on: 2020-08-14 18:08
 """
 
-with open("/Users/zhangqian/jupyer-notebookproject/XGQA/assert/classification_data/" + "all_data" + ".tsv", "r",
+with open("/Users/zhangqian/jupyer-notebookproject/XGQA/assert/train_data/" + "dev" + ".tsv", "r",
           encoding="utf-8") as f:
     question = ""
     numlist = []
     for line in f.readlines():
         str = line.split("\t")[1]
 
-        num = str.split(":")[0][1:]
-        num = int(num)
         # print("{} ----- {} ".format(str, num))
-        if num not in numlist:
-            numlist.append(num)
-        else:
-            print("重复数据： --------------------》  {}", format(num))
+        if str not in numlist:
+            numlist.append(str)
 
-        numlist.sort()
-    print(numlist)
+        else:
+            print("重复数据： --------------------》  {}", format(str))
+
