5af28d4c73b7de57e4a4e6744529c3a773c44041
zhangqian <415677303@qq.com>
2020-08-24 11:17:59 +0800

    问答系统搭建

--- QA_main.py
+++ QA_main.py
@@ -10,7 +10,17 @@
 
 from utils.handle_word import *
 from utils.url_manager import *
+import time
+from models.qa_classification import question_classif
+from models.qa_ner import *
+from gstore.qa_query import *
+import pandas as pd
 
 if __name__ == "__main__":
     questions = get_question_list(question_url)
-    print(questions)
+
+    type_list = []
+    for question in questions:
+        end_classif = question_classif(question)
+        if end_classif not in type_list:
+            type_list.extend(end_classif)
