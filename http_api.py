5156161893a426699a2bfe3ae51820b4e1a3011f
zhangqian <415677303@qq.com>
2021-01-11 21:36:30 +0800

    优化命名实体识别

--- http_api.py
+++ http_api.py
@@ -22,11 +22,11 @@ def predict():
     # question = "电影《沉默的羔羊》是一部什么类型的电影？"
     classification = question_classif(question)
     answer_end, false_entity, true_entity, false_attr, true_attr = simple_res_process("1", question, classification)
-    end_data = "answer_end:{};" \
-               "false_entity:{};" \
-               "true_entity:{};" \
-               "false_attr:{};" \
-               "true_attr:{};".format(answer_end, false_entity, true_entity, false_attr, true_attr)
+    end_data = "命名实体识别结果:{};\n" \
+               "实体消除歧义结果:{};\n" \
+               "获取到相关属性:{};\n" \
+               "属性消除歧义结果:{};\n" \
+               "答案:{};".format(false_entity, true_entity, false_attr, true_attr, answer_end)
 
     return jsonify(data=end_data)
 
